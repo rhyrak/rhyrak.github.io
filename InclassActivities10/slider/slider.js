36ca2d4485ff3d60b239f2f8d5c243db96bd9c29
rhyrak <sgencay88@gmail.com>
2024-05-10 15:44:04 +0300

    add slider activity

---
+++ InclassActivities10/slider/slider.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $("#slider").bxSlider({
+    auto: true,
+    moveSlides: 1,
+    pause: 3000,
+    pager: true,
+    pagerType: "short",
+    minSlides: 1,
+    maxSlides: 1,
+    slideWidth: 250,
+    slideMargin: 10,
+    randomStart: true,
+  });
+  console.log("qweqwdwa");
+});
